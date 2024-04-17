78dbd3fe425f3031c242fb06341575c3707a3eb7
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-04-17 12:34:29 +0200

    Update content

--- components/organisms/testimonial-carousel/index.tsx
+++ components/organisms/testimonial-carousel/index.tsx
@@ -87,34 +87,48 @@ export default function TestimonialCarousel() {
             name="Aleksandra Grzybkowska"
             title="graficzka, przedsiębiorczyni, wielokrotna medalistka Mistrzostw Polski w Jeździectwie"
           >
-            Zanim poznałam Darka, zleciłam stworzenie swojej strony
-            www.smart-equestrian.pl agencji marketingowej. Strona była bardzo
-            słaba, zrobiona z szablonu pomimo zapewnień, że była tworzona od
-            zera. Na koniec firma odmówiła wprowadzenia poprawek - pomimo
-            informacji o moim urlopie, bo rzekomo zgłosiłam je za późno. Kiedy
-            przekazałam projekt tej stronki do Darka - byłam w szoku, że można
-            robić strony aż tak bardzo 1:1. Jestem bardzo zadowolona z przebiegu
-            tego zlecenia. Darek jest przesympatyczny, wyrozumiały i dokładny w
-            tym co robi. Wyrazem mojego zaufania i aptrobaty jest rychły koniec
-            prac nad kolejnym projektem dla firmy Goliat sp. z o.o.! Darek
-            przejął ten projekt po innej osobie, która porzuciła go ze względu
-            na jego skomplikowanie graficzne. Słowem podsumowania - Darek
-            realizuje projekty dokładnie tak jak tego oczekujesz (oraz doradza
-            jeżeli jego zdaniem coś mogłoby wyglądać lepiej). Jest kulturalny,
-            sympatyczny i zdolny. Polecam całym sercem i rozumem!
+            Zanim poznałam Darka, zleciłam stworzenie swojej strony{" "}
+            <a
+              href="https://smart-equestrian.pl"
+              className="text-blue-600"
+              target="_blank"
+            >
+              www.smart-equestrian.pl
+            </a>{" "}
+            agencji marketingowej. Strona była bardzo słaba, zrobiona z szablonu
+            pomimo zapewnień, że była tworzona od zera. Na koniec firma odmówiła
+            wprowadzenia poprawek - pomimo informacji o moim urlopie, bo rzekomo
+            zgłosiłam je za późno. Kiedy przekazałam projekt tej stronki do
+            Darka - byłam w szoku, że można robić strony aż tak bardzo 1:1.
+            Jestem bardzo zadowolona z przebiegu tego zlecenia. Darek jest
+            przesympatyczny, wyrozumiały i dokładny w tym co robi. Wyrazem
+            mojego zaufania i aptrobaty jest rychły koniec prac nad kolejnym
+            projektem dla firmy Goliat sp. z o.o.! Darek przejął ten projekt po
+            innej osobie, która porzuciła go ze względu na jego skomplikowanie
+            graficzne. Słowem podsumowania - Darek realizuje projekty dokładnie
+            tak jak tego oczekujesz (oraz doradza jeżeli jego zdaniem coś
+            mogłoby wyglądać lepiej). Jest kulturalny, sympatyczny i zdolny.
+            Polecam całym sercem i rozumem!
           </TestimonialCard>
           <TestimonialCard
             name="Wojciech Ciesielkiewicz"
             title="Prezes Fundacji Kopalnia Inicjatyw"
           >
-            Nasza Fundacja Kopalnia Inicjatyw działa już od wielu lat. Strona
-            internetowa była w naszych planach już dawno. W końcu nadszedł
-            moment na jej założenie. Zdecydowaliśmy się na Strony jak się patrzy
-            i Pana Dariusza. Wszystko zostało sprawnie ustalone i bardzo
-            przystępnie wytłumaczone. Zostaliśmy kompleksowo zaopiekowani od
-            grafiki po aspekty techniczne. Strona wygląda przejrzyście i
-            nowocześnie. Super kontakt i wsparcie pierwszorzędne ze strony Pana
-            Dariusza.
+            Nasza{" "}
+            <a
+              href="https://kopalniainicjatyw.eu"
+              className="text-blue-600"
+              target="_blank"
+            >
+              Fundacja Kopalnia Inicjatyw
+            </a>{" "}
+            działa już od wielu lat. Strona internetowa była w naszych planach
+            już dawno. W końcu nadszedł moment na jej założenie. Zdecydowaliśmy
+            się na Strony jak się patrzy i Pana Dariusza. Wszystko zostało
+            sprawnie ustalone i bardzo przystępnie wytłumaczone. Zostaliśmy
+            kompleksowo zaopiekowani od grafiki po aspekty techniczne. Strona
+            wygląda przejrzyście i nowocześnie. Super kontakt i wsparcie
+            pierwszorzędne ze strony Pana Dariusza.
           </TestimonialCard>
         </Slider>
       </div>
