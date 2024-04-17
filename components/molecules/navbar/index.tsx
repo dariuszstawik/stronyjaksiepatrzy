d509796989405439527c7b8838960e35e868b09f
Dariusz Stawik <dariusz.stawik@gmail.com>
2024-04-17 21:55:48 +0200

    Fix RWD in navbar

--- components/molecules/navbar/index.tsx
+++ components/molecules/navbar/index.tsx
@@ -19,7 +19,7 @@ const Navbar = ({ isNotHomepage }: { isNotHomepage?: boolean }) => {
   return (
     <div className="fixed w-full h-28 flex justify-between items-center px-4 lg:px-16 py-4 bg-white top-0 left-0 z-30">
       <Logo closeMobileMenu={closeMobileMenu} isNotHomepage={isNotHomepage} />
-      <div className="pl-16 w-3/5 flex justify-end lg:justify-between content-center items-center gap-6">
+      <div className="pl-6 md:pl-16 w-3/5 flex justify-end lg:justify-between content-center items-center gap-6">
         <Navlinks
           isVisible={isMobileMenuActive}
           closeMobileMenu={closeMobileMenu}
